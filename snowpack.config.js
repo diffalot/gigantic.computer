86a85b6e8b2c7173cc669ef663add30c854c8363
Alice Davis <alice@gigantic.computer>
2020-12-28 22:19:59 -0800

    optimize snowpack build

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,5 +3,10 @@ module.exports = {
   plugins: ["@snowpack/plugin-postcss"],
   experiments: {
     source: "skypack",
+    optimize: {
+      bundle: true,
+      minify: true,
+      target: "es2018",
+    },
   },
 };
